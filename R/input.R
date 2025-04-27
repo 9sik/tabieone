329c3026dec37fd9994fabe5201f1c443d3ce976
9sik <pkskvv@gmail.com>
2025-04-27 18:19:56 +0900

    feat: add input function and fix documentation

--- R/input.R
+++ R/input.R
@@ -1,36 +1,3 @@
-#' API 키 암호화 함수
-#'
-#' @param key 암호화할 API 키 문자열
-#' @param password 암호화에 사용할 비밀번호 문자열
-#' @export
-encrypt_api_key <- function(key, password) {
-  library(sodium)
-
-  key_raw <- charToRaw(key)
-  password_raw <- charToRaw(password)
-
-  # salt와 nonce 생성
-  salt <- randombytes(16)
-  nonce <- randombytes(24)
-
-  # 키 암호화
-  encrypted_key <- simple_encrypt(key_raw, password_raw, salt, nonce)
-
-  # inst/extdata 폴더가 없으면 생성
-  if (!dir.exists(file.path("inst", "extdata"))) {
-    dir.create(file.path("inst", "extdata"), recursive = TRUE)
-  }
-
-  # salt, nonce도 함께 저장 (복호화에 필요)
-  saveRDS(list(
-    encrypted_key = encrypted_key,
-    salt = salt,
-    nonce = nonce
-  ), file = file.path("inst", "extdata", "encrypted_key.bin"))
-
-  message("✅ API 키가 성공적으로 암호화되어 저장되었습니다.")
-}
-
 #' API 키 복호화 함수
 #'
 #' @param password 복호화에 사용할 비밀번호 문자열
@@ -38,8 +5,14 @@ encrypt_api_key <- function(key, password) {
 decrypt_api_key <- function(password) {
   library(sodium)
 
-  # 저장된 데이터 읽기
-  encrypted_data <- readRDS(file.path("inst", "extdata", "encrypted_key.bin"))
+  # 설치된 경로에서 파일 읽기 시도
+  encrypted_path <- system.file("extdata", "encrypted_key.bin", package = "tabieone")
+  if (encrypted_path == "") {
+    # 설치된 패키지 경로에 없으면 개발 중 경로 사용
+    encrypted_path <- file.path("inst", "extdata", "encrypted_key.bin")
+  }
+
+  encrypted_data <- readRDS(encrypted_path)
 
   encrypted_key <- encrypted_data$encrypted_key
   salt <- encrypted_data$salt
@@ -47,7 +20,6 @@ decrypt_api_key <- function(password) {
 
   password_raw <- charToRaw(password)
 
-  # 복호화
   decrypted_key <- simple_decrypt(encrypted_key, password_raw, salt, nonce)
 
   return(rawToChar(decrypted_key))
